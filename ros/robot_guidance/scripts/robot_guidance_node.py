eb50744a01bcbc053c8451303038edf069b33824
Yasuo Hayashibara <yasuo.hayashibara@it-chiba.ac.jp>
2017-08-16 17:58:32 +0900

    ros

---
+++ ros/robot_guidance/scripts/robot_guidance_node.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+from std_msgs.msg import String
+import reinforcement_learning as RL
+
+def robot_guidance_node():
+    def __init__(self):
+        self.gpu = rospy.get_param('-gpu', -1)
+        self.model =
+
+if __name__ == '__main__':
+    rospy.init_node('robot_guidance_node')
+    app = robot_guidance_node()
+    rospy.spin()
+
