b49ba5de12452f99bea060faee12d08c68d503b6
mirai <yasuo.hayashibara@it-chiba.ac.jp>
2017-08-22 06:31:55 +0900

    add image_view.py

---
+++ ros/robot_guidance/scripts/image_view.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import rospy
+import cv2
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+class image_view:
+    def __init__(self):
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber("image_raw", Image, self.callback)
+
+    def callback(self, data):
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+
+        cv2.imshow("Image windows", cv_image)
+        cv2.waitKey(3)
+
+if __name__ == '__main__':
+    im = image_view()
+    rospy.init_node('image_view', anonymous=True)
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+    cv2.destroyAllWindows()
--- ros/robot_guidance/scripts/robot_guidance_node.py
+++ ros/robot_guidance/scripts/robot_guidance_node.py
@@ -3,11 +3,16 @@
 import rospy
 from std_msgs.msg import String
 import reinforcement_learning as RL
+import cv_bridge
+import skimage.transform
+
+def callback(imgmsg):
+    bridge = cv_bridge
 
 def robot_guidance_node():
-    def __init__(self):
-        self.gpu = rospy.get_param('-gpu', -1)
-        self.model =
+    rl = RL()
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber('chatter', String, callback)
 
 if __name__ == '__main__':
     rospy.init_node('robot_guidance_node')
