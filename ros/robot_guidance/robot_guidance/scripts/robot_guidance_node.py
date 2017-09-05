b9450a99a5f32aa801850d2c520e7f3a7cdd0e9a
pethon <pasin_th@hotmail.com>
2017-09-05 14:20:28 +0900

    fix bug with camera

--- ros/robot_guidance/robot_guidance/scripts/robot_guidance_node.py
+++ ros/robot_guidance/robot_guidance/scripts/robot_guidance_node.py
@@ -26,6 +26,8 @@ class robot_guidance_node:
         self.action = 0
         self.reward = 0
         self.poten = Adc()
+        self.t0 = rospy.Time.now().to_sec()
+        self.t1 = 0
 
     def callback(self, data):
         try:
@@ -41,12 +43,30 @@ class robot_guidance_node:
         r, g, b = cv2.split(img)
         imgobj = np.asanyarray([r,g,b])
 #        print(imgobj)
-        self.reward = 2.0 - (self.poten.adc0 + self.poten.adc1 + self.poten.adc2*2 + self.poten.adc3*2)/100.0
-        self.action = self.rl.act_and_trains(imgobj, self.reward)
-        self.action_pub.publish(self.action)
 
-        print("action: " +str(self.action) + ", reward: " + str(self.reward))
-        time.sleep(1)
+        self.t1 = rospy.Time.now().to_sec()
+        if self.t1 >= self.t0 + 1:
+            self.reward = 2.0 - (self.poten.adc0 + self.poten.adc1 + self.poten.adc2*2 + self.poten.adc3*2)/100.0
+            self.action = self.rl.act_and_trains(imgobj, self.reward)
+            self.action_pub.publish(self.action)
+
+            if self.action == 1:
+                move = 'Right'
+            elif self.action == 2:
+                move = 'Front'
+            elif self.action == 3:
+                move = 'Left'
+            elif self.action == 4:
+                move = 'Stop'
+            elif self.action == 0:
+                move = 'Stop'
+
+            if self.reward >= 1.9:
+                print("action: " + move + "\x1b[6;30;42m" " reward: " + str(self.reward) + "\x1b[0m")
+            elif self.reward < 1.9:
+                print("action: " + move + "\x1b[6;30;41m" " reward: " + str(self.reward) + "\x1b[0m")
+            self.t0 = rospy.Time.now().to_sec()
+
 
     def callback_poten(self, data):
         self.poten = data
