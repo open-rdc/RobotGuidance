c3e7ab75ed88b3b2bc24389650c5643b069dbb3b
mirai <yasuo.hayashibara@it-chiba.ac.jp>
2017-09-11 07:52:37 +0900

    modify reinforcement_learning.py

--- ros/robot_guidance/robot_guidance/scripts/reinforcement_learning.py
+++ ros/robot_guidance/robot_guidance/scripts/reinforcement_learning.py
@@ -3,6 +3,8 @@ import chainer
 import chainer.functions as F
 import chainer.links as L
 import numpy as np
+import os
+from os.path import expanduser
 
 class QFunction(chainer.Chain):
     def __init__(self, n_history=3, n_action=5):
@@ -27,7 +29,7 @@ class QFunction(chainer.Chain):
 class reinforcement_learning:
     def __init__(self, n_history=3, n_action=5):
         self.q_func = QFunction(n_history, n_action)
-#        self.q_func.to_gpu()
+        self.q_func.to_gpu()
         self.optimizer = chainer.optimizers.Adam(eps=1e-2)
         self.optimizer.setup(self.q_func)
         self.gamma = 0.95
@@ -41,12 +43,22 @@ class reinforcement_learning:
             minibatch_size=4, replay_start_size=500, update_interval=1,
             target_update_interval=100, phi=self.phi)
 
+        home = expanduser("~")
+        if os.path.isdir(home + '/agent'):
+            self.agent.load('agent')
+            print('Load agent')
+
     def act_and_trains(self, obs, reward):
         self.action = self.agent.act_and_train(obs, reward)
         return self.action
+
+    def save_agent(self):
+        self.agent.save('agent')
+#        print('Save agent')
 
     def action_space_sample(self):
         return np.random.randint(1,self.n_action)
 
 if __name__ == '__main__':
     rl = reinforcement_learning()
+
