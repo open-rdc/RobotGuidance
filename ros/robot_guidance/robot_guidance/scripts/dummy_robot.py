a09f46ff1e25d3410ccf6feb13cce45a3ea9ba4e
mirai <yasuo.hayashibara@it-chiba.ac.jp>
2017-09-19 08:11:15 +0900

    modify dummy_robot.py

--- ros/robot_guidance/robot_guidance/scripts/dummy_robot.py
+++ ros/robot_guidance/robot_guidance/scripts/dummy_robot.py
@@ -13,8 +13,8 @@ class dummy_robot:
     def __init__(self):
         rospy.init_node('dummy_robot', anonymous=True)
         self.bridge = CvBridge()
-        self.image_pub = rospy.Publisher("image_raw", Image, queue_size=10)
-        self.reward_pub = rospy.Publisher("reward", Float32, queue_size=10)
+        self.image_pub = rospy.Publisher("image_raw", Image, queue_size=1)
+        self.reward_pub = rospy.Publisher("reward", Float32, queue_size=1)
         self.action_sub = rospy.Subscriber("action", Int8, self.callback_action)
         self.action = 0
         self.pan = 0
@@ -22,10 +22,16 @@ class dummy_robot:
         self.cv_image = np.zeros((480,640,3), np.uint8)
         self.cv_image.fill(255)
         self.image = self.bridge.cv2_to_imgmsg(self.cv_image, encoding="bgr8")
-        self.timer = rospy.Timer(rospy.Duration(0.1), self.callback_timer)
+        self.timer = rospy.Timer(rospy.Duration(0.033), self.callback_timer)
+        self.count = 0
 
     def callback_timer(self, data):
+#        print('Timer called at ' + str(data.current_real))
         self.cv_image.fill(255)
+        self.count += 1
+        if ((self.count % 100) == 0):
+            self.pan = int(np.random.rand() * 400 - 200)
+            print("change pan angle")
         cv2.circle(self.cv_image, (640 / 2 + self.pan, 480 / 2), 200, (0, 255, 0), -1)
         self.image = self.bridge.cv2_to_imgmsg(self.cv_image, encoding="bgr8")
         self.image_pub.publish(self.image)
@@ -38,7 +44,7 @@ class dummy_robot:
         self.pan += action_list[self.action]
         self.reward = 1.0 - abs(self.pan) / 100.0
 
-        print("selected_action: " + str(self.action) + ", reward: " + str(self.reward))
+#        print("selected_action: " + str(self.action) + ", reward: " + str(self.reward))
         self.reward_pub.publish(self.reward)
 
 if __name__ == '__main__':
