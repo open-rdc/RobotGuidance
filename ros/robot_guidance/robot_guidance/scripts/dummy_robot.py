9d203d42b77a18d4856bc88673cabcfe549e0edc
yasuohayashibara <yasuo.hayashibara@it-chiba.ac.jp>
2017-09-18 20:51:45 +0900

    add dummy_robot.py

---
+++ ros/robot_guidance/robot_guidance/scripts/dummy_robot.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import roslib
+roslib.load_manifest('robot_guidance')
+import rospy
+import cv2
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+from std_msgs.msg import Float32, Int8
+import numpy as np
+
+class dummy_robot:
+    def __init__(self):
+        rospy.init_node('dummy_robot', anonymous=True)
+        self.bridge = CvBridge()
+        self.image_pub = rospy.Publisher("image_raw", Image, queue_size=10)
+        self.reward_pub = rospy.Publisher("reward", Float32, queue_size=10)
+        self.action_sub = rospy.Subscriber("action", Int8, self.callback_action)
+        self.action = 0
+        self.pan = 0
+        self.reward = 0
+        self.cv_image = np.zeros((480,640,3), np.uint8)
+        self.image = self.bridge.cv2_to_imgmsg(self.cv_image, encoding="bgr8")
+        self.timer = rospy.Timer(rospy.Duration(0.1), self.callback_timer)
+
+    def callback_timer(self, data):
+        cv2.circle(self.cv_image, (640 / 2 + self.pan, 480 / 2), 200, (0, 255, 0), -1)
+        self.image = self.bridge.cv2_to_imgmsg(self.cv_image, encoding="bgr8")
+        self.image_pub.publish(self.image)
+
+    def callback_action(self, data):
+        action_list = [-10, 0, 10]
+        if (self.data < 0 or self.data >= 3):
+            return
+        self.action = data.data
+        self.pan += action_list[self.action]
+        self.reward = 1.0 - self.pan / 100.0
+
+        print("selected_action: " + str(self.action) + ", reward: " + str(self.reward))
+        self.reward_pub.publish(self.reward)
+
+if __name__ == '__main__':
+    dr = dummy_robot()
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting Down")
+    cv2.destroyAllWindows()
