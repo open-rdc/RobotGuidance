75c5fbe9a3cc25ee52f7de5c933271097db3d39f
MasayaOkada <masa.okd888@gmail.com>
2019-07-20 16:32:18 +0900

    fix

--- ros/robot_guidance/robot_guidance/scripts/controller.py
+++ ros/robot_guidance/robot_guidance/scripts/controller.py
@@ -3,14 +3,14 @@
 # rostopic pub -1 /adc rosserial_arduino/Adc 0 0 0 0 0 0
 
 import rospy
-from std_msgs.msg import Int8
+from std_msgs.msg import Int32
 from rosserial_arduino.msg import Adc
 
 class robot_controller:
 	def __init__(self):
 		rospy.init_node('robot_controller', anonymous=True)
 		self.poten_sub = rospy.Subscriber("/adc", Adc, self.callback_poten)
-		self.control_pub = rospy.Publisher('/control', Int8, queue_size=10)
+		self.control_pub = rospy.Publisher('/control', Int32, queue_size=10)
 		self.poten = Adc()
 #		self.action = 0
 #		self.pitch = 0
--- ros/robot_guidance/robot_guidance/scripts/machine_learning_node.py
+++ ros/robot_guidance/robot_guidance/scripts/machine_learning_node.py
@@ -8,7 +8,7 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from deep_learning import *
 from skimage.transform import resize
-from std_msgs.msg import Int8
+from std_msgs.msg import Int32, Int8
 import sys
 import skimage.transform
 import csv
@@ -24,7 +24,7 @@ class machine_learning_node:
 		self.dl = deep_learning(n_action = self.action_num)
 		self.bridge = CvBridge()
 		self.image_sub = rospy.Subscriber("/image_raw", Image, self.callback)
-		self.control_sub = rospy.Subscriber("/control", Int8, self.callback_learning)
+		self.control_sub = rospy.Subscriber("/control", Int32, self.callback_learning)
                 self.action_pub = rospy.Publisher("action", Int8, queue_size=1)
 		self.action = 0
 		self.correct_action = 0
@@ -66,30 +66,24 @@ class machine_learning_node:
 			self.learning = False
 		else:
 			self.learning = True
-
-
-		ros_time = str(rospy.Time.now())
+		
+                ros_time = str(rospy.Time.now())
 		if self.learning:
 			self.count += 1
 			self.action = self.dl.act_and_trains(imgobj, self.correct_action)
-
+                
 			line = [ros_time, str(self.correct_action), str(self.action)]
 			with open(self.path + self.start_time + '/' +  'action.csv', 'a') as f:
 				writer = csv.writer(f, lineterminator='\n')
 				writer.writerow(line)
+		        self.action_pub.publish(self.correct_action)
+                
+                
+                else:
+                        self.action = self.dl.act(imgobj)
+                        self.action_pub.publish(self.action)
 
-#		else:
-#			self.action = self.rl.act(imgobj)
-		self.action_pub.publish(self.correct_action)
-
-#		cv2.putText(self.cv_image,self.action_list[self.action],(550,450), cv2.FONT_HERSHEY_SIMPLEX, 1,(0,0,255),2)
-#		image_name = self.path + self.start_time + '/' + ros_time + '.png'
-#		cv2.imwrite(image_name, self.cv_image)
-#                print(imgobj)
-#		self.correct_ratio = 0.97 * self.correct_ratio + 0.03 * self.correct
 		print("learning = " + str(self.learning) + " count: " + str(self.count) + " correct_action: " + str(self.correct_action) + " action: " + str(self.action))
-#		if((self.count - 1) % 100 == 0 and self.count > 100):
-#			self.rl.save_agent()
 
 
 if __name__ == '__main__':
