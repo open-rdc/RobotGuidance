c32fdac2e623b92da5a6112f442f0b2bab533a94
MasayaOkada <masa.okd888@gmail.com>
2019-07-23 15:24:34 +0900

    add controller

--- ros/robot_guidance/robot_guidance/scripts/robot_guidance_node.py
+++ ros/robot_guidance/robot_guidance/scripts/robot_guidance_node.py
@@ -36,7 +36,6 @@ class robot_guidance_node:
 		self.learning = True
 		self.start_time = time.strftime("%Y%m%d_%H:%M:%S")
 		self.action_list = ['Front', 'Right', 'Left', 'Back', 'Stop']
-		#self.path = '/home/pete/cit-1808/research_pic/'
 		self.path = 'cit-1808/research_pic/'
 		os.makedirs(self.path + self.start_time)
 
