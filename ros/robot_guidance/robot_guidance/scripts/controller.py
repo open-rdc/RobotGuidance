cdf46b706c85afddc48e56035e328d46e398e38f
masaya <masa.okd888@gmail.com>
2019-07-30 12:15:11 +0900

    control action by potentiometer

---
+++ ros/robot_guidance/robot_guidance/scripts/controller.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# check node
+# rostopic pub -1 /adc rosserial_arduino/Adc 0 0 0 0 0 0
+
+import rospy
+from std_msgs.msg import Int32
+from rosserial_arduino.msg import Adc
+
+class robot_controller:
+	def __init__(self):
+		rospy.init_node('robot_controller', anonymous=True)
+		self.poten_sub = rospy.Subscriber("/adc", Adc, self.callback_poten)
+		self.control_pub = rospy.Publisher('/control', Int32, queue_size=10)
+		self.poten = Adc()
+#		self.pitch = 0
+#		self.yaw = 0
+
+	def callback_poten(self, data):
+		self.poten = data
+		self.control = 0
+		self.correct_action = 0
+
+		if self.poten.adc2 > 120:
+			#	for testing
+			self.correct_action = -1
+		else:
+			if self.poten.adc1 > 10:
+				self.correct_action = 2
+			elif self.poten.adc0 > 10:
+				self.correct_action = 1
+			else:
+				self.correct_action = 0
+
+		#Publish the correct_action
+		self.control_pub.publish(self.correct_action)
+
+if __name__ == '__main__':
+	rc = robot_controller()
+	try:
+		rospy.spin()
+	except KeyboardInterrupt:
+		print("Shutting Down")
