2f903f94203522d4188d766f0243310c417b8d47
MasayaOkada <masa.okd888@gmail.com>
2019-09-23 20:36:01 +0900

    add result

--- ros/robot_guidance/robot_guidance/scripts/deep_learning.py
+++ ros/robot_guidance/robot_guidance/scripts/deep_learning.py
@@ -4,13 +4,14 @@ import chainer.links as L
 from chainer import Chain, variable
 from chainer.datasets import TupleDataset
 from chainer.iterators import SerialIterator
+from chainer.optimizer_hooks import WeightDecay
 import numpy as np
 import matplotlib as plt
 import os
 from os.path import expanduser
 
 # HYPER PARAM
-BATCH_SIZE = 32
+BATCH_SIZE = 1
 
 class Net(chainer.Chain):
     def __init__(self, n_channel=3, n_action=3):
@@ -19,7 +20,7 @@ class Net(chainer.Chain):
             conv1=L.Convolution2D(n_channel, 32, ksize=8, stride=4, nobias=False, initialW=initializer),
             conv2=L.Convolution2D(32, 64, ksize=3, stride=2, nobias=False, initialW=initializer),
             conv3=L.Convolution2D(64, 64, ksize=3, stride=1, nobias=False, initialW=initializer),
-            conv4=L.Linear(960, 512, initialW=initializer),
+            fc4=L.Linear(960, 512, initialW=initializer),
             fc5=L.Linear(512, n_action, initialW=np.zeros((n_action, 512), dtype=np.int32))
         )
 
@@ -28,7 +29,7 @@ class Net(chainer.Chain):
         h1 = F.relu(self.conv1(s))
         h2 = F.relu(self.conv2(h1))
         h3 = F.relu(self.conv3(h2))
-        h4 = F.relu(self.conv4(h3))
+        h4 = F.relu(self.fc4(h3))
         h = self.fc5(h4)
         return h
 
@@ -37,11 +38,15 @@ class deep_learning:
         self.net = Net(n_channel, n_action)
         self.optimizer = chainer.optimizers.Adam(eps=1e-2)
         self.optimizer.setup(self.net)
+        self.optimizer.add_hook(chainer.optimizer.WeightDecay(5e-4))
         self.n_action = n_action
         self.phi = lambda x: x.astype(np.float32, copy=False)
         self.count = 0
+        self.accuracy = 0
         self.results_train = {}
         self.results_train['loss'], self.results_train['accuracy'] = [], []
+        self.loss_list = []
+        self.acc_list = []
 
     def act_and_trains(self, imgobj, correct_action):
       
@@ -56,6 +61,10 @@ class deep_learning:
 
             loss_train = F.softmax_cross_entropy(y_train, t_train)
             acc_train = F.accuracy(y_train, t_train)
+
+            self.loss_list.append(loss_train.array)
+            self.acc_list.append(acc_train.array)
+
             self.net.cleargrads()
             loss_train.backward()
             self.optimizer.update()
@@ -66,8 +75,8 @@ class deep_learning:
             self.results_train['accuracy'] .append(acc_train.array)
 
             action = np.argmax(y_train.array)
-            
-            print('iteration: {}, loss (train): {:.4f}, acc (train): {:.4f}, action: {}'.format(self.count, loss_train.array.mean(), acc_train.array.mean(), action))
+            self.accuracy = np.mean(self.acc_list)
+            print('iteration: {}, acc (train): {:.4f}, action: {}'.format(self.count, self.accuracy, action))
             
             return action
 
@@ -79,6 +88,12 @@ class deep_learning:
                 action_value = self.net(x_test)
                 action = np.argmax(action_value.array)
             return action
+
+
+
+    def result(self):
+            accuracy = self.accuracy
+            return accuracy
                 
 if __name__ == '__main__':
         dl = deep_learning()
